feb3462752053ae03383e0ef599e4ced42408bc5
Swift7994 <JSwift7994@googlemail.com>
2024-12-08 12:19:02 +0000

    add sprites and player objects

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
+    # initialize pygame and set screen size
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # prints in the terminal when the file is executed
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # instantiate a Player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # create a clock object and set it to "clock" variable
     clock = pygame.time.Clock()
+    # initialize a value "delta time", which stores the value the .tick() method returns
     dt = 0
-    while True:
+    # infinite game loop
+    running = True
+    while running:
+        # checks if the user tries to exit the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0), rect=None)
+        # fills the screen black (game background)
+        screen.fill("black", rect=None)
+        # renders the player object to the screen
+        player.draw(screen)
+        # refreshes the screen
         pygame.display.flip()
+        # limits the FPS for the game and updates the dt variable
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
